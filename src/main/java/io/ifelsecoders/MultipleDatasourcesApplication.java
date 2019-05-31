7bb9589a62d4adc65c9f3fb5a4cc7585cb57dcdf
Oleksii Kondratiuk <kondratyuk.alex@gmail.com>
2019-05-31 11:12:20 +0300

    Created simple spring boot application.

---
+++ src/main/java/io/ifelsecoders/MultipleDatasourcesApplication.java
@@ -0,0 +1,11 @@
+package io.ifelsecoders;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleDatasourcesApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
